dbd9d936607b932f5731c22cbf4388d124d14900
ksgwr <ksgwarai@gmail.com>
2018-07-21 20:06:18 +0900

    add similarity

--- src/main/java/jp/ksgwr/charlooksim/CharacterLooksSimilarity.java
+++ src/main/java/jp/ksgwr/charlooksim/CharacterLooksSimilarity.java
@@ -42,6 +42,80 @@ public class CharacterLooksSimilarity {
         return charLooks;
     }
 
+    public static float calcSimilarity(int[][] a, int[][] b) {
+        int maxX;
+        // set long width array and short width array
+        int[][] lw, sw;
+        if (a[0].length < b[0].length) {
+            maxX = b[0].length;
+            lw = b;
+            sw = a;
+        } else {
+            maxX = a[0].length;
+            lw = a;
+            sw = b;
+        }
+
+        // set short array centering
+        int maxY;
+        int cxStartX = (lw[0].length - sw[0].length) / 2;
+        int cxEndX = (lw[0].length + sw[0].length) / 2;
+        int cxStartY, cxEndY;
+        if (a.length < b.length) {
+            maxY = b.length;
+            cxStartY = (b.length - a.length) / 2;
+            cxEndY = (b.length + a.length) / 2;
+        } else {
+            maxY = a.length;
+            cxStartY = (a.length - b.length) / 2;
+            cxEndY = (a.length + b.length) / 2;
+        }
+
+        int diff = 0;
+
+        for (int i=0;i<lw.length;i++) {
+            for(int j=0;j<lw[i].length;j++) {
+                if (cxStartX <= j && j <= cxEndX) {
+                    if (cxStartY <= i && i <= cxEndY) {
+                        // cross array
+                        if (lw[i][j] != sw[i - cxStartY][j - cxStartX]) {
+                            diff++;
+                        }
+                    } else {
+                        // out of short height array as 0
+                        if (lw[i][j] == 1) {
+                            diff++;
+                        }
+                    }
+                } else {
+                    // out of short width array as 0
+                    if (lw[i][j] == 1) {
+                        diff++;
+                    }
+                }
+            }
+        }
+        if (lw.length < sw.length) {
+            // out of short height array as 0
+            for (int i=0;i<cxStartY;i++) {
+                for(int j=0;j<sw[i].length;j++) {
+                    if (sw[i][j] == 1) {
+                        diff++;
+                    }
+                }
+            }
+            for (int i=cxEndY+1;i<sw.length;i++) {
+                for(int j=0;j<sw[i].length;j++) {
+                    if (sw[i][j] == 1) {
+                        diff++;
+                    }
+                }
+            }
+        }
+
+        return 1.0f - (float) diff / (maxX * maxY);
+    }
+
 }
            
       
--- src/test/java/jp/ksgwr/charlooksim/CharacterLooksSimilarityTest.java
+++ src/test/java/jp/ksgwr/charlooksim/CharacterLooksSimilarityTest.java
@@ -1,6 +1,9 @@
 package jp.ksgwr.charlooksim;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CharacterLooksSimilarityTest {
 
@@ -14,5 +17,33 @@ public class CharacterLooksSimilarityTest {
             }
             System.out.println();
         }
+    }
+
+    @Test
+    public void calcSimilarityTest() {
+        int[][] a = CharacterLooksSimilarity.createCharacterLooks("a");
+        int[][] b = CharacterLooksSimilarity.createCharacterLooks("b");
+
+        float sim = CharacterLooksSimilarity.calcSimilarity(a, b);
+
+        assertEquals(sim, 0.89f, 0.01f);
+
+        int[][] l = CharacterLooksSimilarity.createCharacterLooks("l");
+        int[][] i = CharacterLooksSimilarity.createCharacterLooks("i");
+
+        sim = CharacterLooksSimilarity.calcSimilarity(l, i);
+
+        assertEquals(sim, 0.98f, 0.01f);
+
+        int[][] I = CharacterLooksSimilarity.createCharacterLooks("I");
+
+        sim = CharacterLooksSimilarity.calcSimilarity(l, I);
+
+        assertEquals(sim, 1.0f, 0.01f);
+
+        sim =  CharacterLooksSimilarity.calcSimilarity(l, l);
+
+        assertEquals(sim, 1.0f, 0.01f);
+
     }
 }
