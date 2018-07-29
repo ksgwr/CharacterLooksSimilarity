a2e45bf0be9cbfb028dbfd574d7b09dd20afa441
ksgwr <ksgwarai@gmail.com>
2018-07-29 14:37:55 +0900

    set IPA font

--- src/main/java/jp/ksgwr/charlooksim/CharacterLooksSimilarity.java
+++ src/main/java/jp/ksgwr/charlooksim/CharacterLooksSimilarity.java
@@ -42,6 +42,32 @@ public class CharacterLooksSimilarity {
     }
 
     public static float calcSimilarity(int[][] a, int[][] b) {
+        // lやIの境界条件を考慮してx軸方向で2つ探索して良い方を取る
+        return calcSimilarity(a, b, 0, 2, 0, 1);
+    }
+
+    public static float calcSimilarity(int[][] a, int[][] b, int startInclusiveDeltaX, int endExclusiveDeltaX, int startInclusiveDeltaY, int endExclusiveDeltaY) {
+        int maxX = Math.max(a[0].length, b[0].length);
+        int maxY = Math.max(a.length, b.length);
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i=startInclusiveDeltaY;i<endExclusiveDeltaY;i++) {
+            for (int j = startInclusiveDeltaX; j < endExclusiveDeltaX; j++) {
+                int diff = countDifference(a, b, j, i);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                }
+            }
+        }
+
+        return 1.0f - (float) minDiff / (maxX * maxY);
+    }
+
+    public static int countDifference(int[][] a, int[][] b) {
+        return countDifference(a, b, 0, 0);
+    }
+
+    public static int countDifference(int[][] a, int[][] b, int deltaX, int deltaY) {
         int maxX;
         // set long width array and short width array
         int[][] lw, sw;
@@ -57,23 +83,23 @@ public class CharacterLooksSimilarity {
 
         // set short array centering
         int maxY;
-        int cxStartX = (lw[0].length - sw[0].length) / 2;
-        int cxEndX = (lw[0].length + sw[0].length) / 2;
+        int cxStartX = (lw[0].length - sw[0].length ) / 2 + deltaX;
+        int cxEndX = (lw[0].length + sw[0].length ) / 2 + deltaX;
         int cxStartY, cxEndY;
         if (a.length < b.length) {
             maxY = b.length;
-            cxStartY = (b.length - a.length) / 2;
-            cxEndY = (b.length + a.length) / 2;
+            cxStartY = (b.length - a.length ) / 2 + deltaY;
+            cxEndY = (b.length + a.length ) / 2 + deltaY;
         } else {
             maxY = a.length;
-            cxStartY = (a.length - b.length) / 2;
-            cxEndY = (a.length + b.length) / 2;
+            cxStartY = (a.length - b.length) / 2 + deltaY;
+            cxEndY = (a.length + b.length) / 2 + deltaY;
         }
 
         int diff = 0;
 
-        for (int i=0;i<lw.length;i++) {
-            for(int j=0;j<lw[i].length;j++) {
+        for (int i = 0; i < lw.length; i++) {
+            for (int j = 0; j < lw[i].length; j++) {
                 if (cxStartX <= j && j < cxEndX) {
                     if (cxStartY <= i && i < cxEndY) {
                         // cross array
@@ -96,15 +122,15 @@ public class CharacterLooksSimilarity {
         }
         if (lw.length < sw.length) {
             // out of short height array as 0
-            for (int i=0;i<cxStartY;i++) {
-                for(int j=0;j<sw[i].length;j++) {
+            for (int i = 0; i < cxStartY; i++) {
+                for (int j = 0; j < sw[i].length; j++) {
                     if (sw[i][j] == 1) {
                         diff++;
                     }
                 }
             }
-            for (int i=cxEndY+1;i<sw.length;i++) {
-                for(int j=0;j<sw[i].length;j++) {
+            for (int i = cxEndY + 1; i < sw.length; i++) {
+                for (int j = 0; j < sw[i].length; j++) {
                     if (sw[i][j] == 1) {
                         diff++;
                     }
@@ -112,7 +138,7 @@ public class CharacterLooksSimilarity {
             }
         }
 
-        return 1.0f - (float) diff / (maxX * maxY);
+        return diff;
     }
 
 }
--- src/test/java/jp/ksgwr/charlooksim/CharacterLooksSimilarityTest.java
+++ src/test/java/jp/ksgwr/charlooksim/CharacterLooksSimilarityTest.java
@@ -9,46 +9,41 @@ import static org.junit.Assert.assertEquals;
 
 public class CharacterLooksSimilarityTest {
 
+    /** download IPAGothic from https://ipafont.ipa.go.jp/old/ipafont/download.html */
+    private final Font FONT = new Font("IPAGothic", Font.PLAIN, 12);
+
+    @Test
+    public void fontTest() {
+        assertEquals("IPAゴシック", FONT.getFamily());
+    }
+
     @Test
     public void printcreateCharacterLooksTest() {
-        Font font = new Font("IPAGothic", Font.PLAIN, 12);
+        int[][] actual = CharacterLooksSimilarity.createCharacterLooks("あ", FONT);
 
-        int[][] actual = CharacterLooksSimilarity.createCharacterLooks("あ");
-
-        for (int i = 0; i < actual.length; i++) {
-            for (int j = 0; j < actual[i].length; j++) {
-                System.out.print(actual[i][j] + ",");
-            }
-            System.out.println();
-        }
+        printCharacter(actual);
     }
 
     @Test
     public void createCharacterLooksTest() {
-        int[][] actual = CharacterLooksSimilarity.createCharacterLooks("y");
+        int[][] actual = CharacterLooksSimilarity.createCharacterLooks("y", FONT);
+
+        printCharacter(actual);
 
         int[][] expected = new int[][]{
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {1, 0, 0, 0, 1,},
-                {1, 0, 0, 0, 1,},
-                {0, 1, 0, 1, 0,},
-                {0, 1, 0, 1, 0,},
-                {0, 1, 0, 1, 0,},
-                {0, 0, 1, 0, 0,},
-                {0, 0, 1, 0, 0,},
-                {0, 0, 1, 0, 0,},
-                {0, 1, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,},
-                {0, 0, 0, 0, 0,}
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 0},
+                {0, 0, 1, 0, 1, 0},
+                {0, 0, 1, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0, 0},
+                {0, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}
         };
 
         assertArrayEquals(expected, actual);
@@ -57,29 +52,83 @@ public class CharacterLooksSimilarityTest {
 
     @Test
     public void calcSimilarityTest() {
-        int[][] a = CharacterLooksSimilarity.createCharacterLooks("a");
-        int[][] b = CharacterLooksSimilarity.createCharacterLooks("b");
+        int[][] a = CharacterLooksSimilarity.createCharacterLooks("a", FONT);
+        int[][] b = CharacterLooksSimilarity.createCharacterLooks("b", FONT);
 
         float sim = CharacterLooksSimilarity.calcSimilarity(a, b);
 
-        assertEquals(0.89f, sim, 0.01f);
+        assertEquals(0.83f, sim, 0.01f);
 
-        int[][] l = CharacterLooksSimilarity.createCharacterLooks("l");
-        int[][] i = CharacterLooksSimilarity.createCharacterLooks("i");
+        int[][] l = CharacterLooksSimilarity.createCharacterLooks("l", FONT);
+        int[][] i = CharacterLooksSimilarity.createCharacterLooks("i", FONT);
 
         sim = CharacterLooksSimilarity.calcSimilarity(l, i);
 
-        assertEquals(0.98f, sim, 0.01f);
+        assertEquals(0.94f, sim, 0.01f);
 
-        int[][] I = CharacterLooksSimilarity.createCharacterLooks("I");
+        int[][] I = CharacterLooksSimilarity.createCharacterLooks("I", FONT);
 
         sim = CharacterLooksSimilarity.calcSimilarity(l, I);
 
-        assertEquals(0.99f, sim, 0.01f);
+        assertEquals(0.94f, sim, 0.01f);
 
         sim = CharacterLooksSimilarity.calcSimilarity(l, l);
 
         assertEquals(1.0f, sim, 0.01f);
 
+    }
+
+    @Test
+    public void countDifferenceTest() {
+        int[][] a = new int[][]{
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        int[][] ua = new int[][]{
+                {1},
+                {1},
+                {1},
+                {0},
+                {0},
+        };
+        int[][] da = new int[][]{
+                {0},
+                {0},
+                {1},
+                {1},
+                {1},
+        };
+        int[][] ra = new int[][]{
+                {0, 0, 1, 1, 1},
+        };
+        int[][] la = new int[][]{
+                {1, 1, 1, 0, 0},
+        };
+
+        int diff = CharacterLooksSimilarity.countDifference(a, ua, 0, 1);
+
+        assertEquals(2, diff);
+
+        diff = CharacterLooksSimilarity.countDifference(a, da, 0, -1);
+        assertEquals(2, diff);
+
+        diff = CharacterLooksSimilarity.countDifference(a, ra, -1, 0);
+        assertEquals(2, diff);
+
+        diff = CharacterLooksSimilarity.countDifference(a, la, 1, 0);
+        assertEquals(2, diff);
+    }
+
+    private void printCharacter(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + ",");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
